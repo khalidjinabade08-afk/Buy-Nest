a63c3546630c86d33cb50312e241085a310d9b24
khalidjinabade08-afk <khalidjinabade08@gmail.com>
2026-08-10 16:45:25 +0530

    add pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,109 +1,119 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import image from "./images/loging-page.jpg";
 
 function App() {
   return (
     <>
-    <div className="min-h-screen border flex items-center justify-center bg-[#f3f3f6]">
-      <div className="w-255 h-125 rounded-3xl border border-amber-700 shadow-lg overflow-hidden bg-[#ffffff] bg-cover bg-center">
-        <div className="flex p-4 items-center text-3xl font-bold text-[#111827]">
+      <div className="min-h-screen flex items-center justify-center bg-[#F8F7F4] p-4">
+
+        <div className="w-full max-w-225 h-137.5 rounded-3xl shadow-lg overflow-hidden bg-[#FFFFFF] flex">
+
+          <div className="w-1/2 p-10">
+
+            <div className="flex items-center text-3xl font-bold text-[#111827] mb-8">
               Create new account
-          <span className="ml-1 mt-2 w-2 h-2 rounded-full bg-[#2DD4BF]"></span>
-        </div>
+              <span className="ml-1 mt-2 w-2 h-2 rounded-full bg-[#2DD4BF]"></span>
+            </div>
 
-        <div className="mt-1 p-2">
-          <div className="relative w-80">
-            <input
-              type="text"
-              id="name"
-              placeholder=" "
-              className="peer w-80 h-13 rounded-lg border border-[#d9dedd] bg-white px-4 py-3 text-[#333333] outline-none transition-all focus:border-[#0f817a] focus:shadow-[0px_0px_4px_rgba(15,129,122,0.25)]"
-            />
+            <div className="mt-1 p-2">
+              <div className="relative w-80">
+                <input
+                  type="text"
+                  id="name"
+                  placeholder=" "
+                  className="peer w-80 h-13 rounded-lg border border-[#d9dedd] bg-white px-4 py-3 text-[#333333] outline-none transition-all focus:border-[#0f817a]! focus:shadow-[0_0_0_3px_rgba(15,129,122,0.15)] "
+                />
 
-            <label
-              htmlFor="name"
-              className="absolute left-4 top-1/2 -translate-y-1/2 bg-white px-1 text-[#777777] transition-all
-              peer-focus:top-0
-              peer-focus:text-sm
-              peer-focus:text-[#0f817a]
-              peer-not-placeholder-shown:top-0`
-              peer-not-placeholder-shown:text-sm"
-            >
-              Enter your name
-            </label>
+                <label
+                  htmlFor="name"
+                  className="absolute left-4 top-1/2 -translate-y-1/2 bg-white px-1 text-[#777777] transition-all
+                  peer-focus:top-0
+                  peer-focus:text-sm
+                  peer-focus:text-[#0f817a]
+                  peer-not-placeholder-shown:top-0
+                  peer-not-placeholder-shown:text-sm"
+                >
+                  Enter your name
+                </label>
+              </div>
+            </div>
+
+            <div className="mt-1 p-2">
+              <div className="relative w-80">
+                <input
+                  type="email"
+                  id="email"
+                  placeholder=" "
+                  className="peer w-80 h-13 rounded-lg border border-[#d9dedd] bg-white px-4 py-3 text-[#333333] outline-none transition-all focus:border-[#0f817a]! focus:shadow-[0_0_0_3px_rgba(15,129,122,0.15)] "
+                />
+
+                <label
+                  htmlFor="email"
+                  className="absolute left-4 top-1/2 -translate-y-1/2 bg-white px-1 text-[#777777] transition-all
+                  peer-focus:top-0
+                  peer-focus:text-sm
+                  peer-focus:text-[#0f817a]
+                  peer-not-placeholder-shown:top-0
+                  peer-not-placeholder-shown:text-sm"
+                >
+                  Enter your email
+                </label>
+              </div>
+            </div>
+
+            <div className="mt-1 p-2">
+              <div className="relative w-80">
+                <input
+                  type="password"
+                  id="password"
+                  placeholder=" "
+                  className="peer w-80 h-13 rounded-lg border border-[#d9dedd] bg-white px-4 py-3 text-[#333333] outline-none transition-all focus:border-[#0f817a]! focus:shadow-[0_0_0_3px_rgba(15,129,122,0.15)] "
+                />
+
+                <label
+                  htmlFor="password"
+                  className="absolute left-4 top-1/2 -translate-y-1/2 bg-white px-1 text-[#777777] transition-all
+                  peer-focus:top-0
+                  peer-focus:text-sm
+                  peer-focus:text-[#0f817a]
+                  peer-not-placeholder-shown:top-0
+                  peer-not-placeholder-shown:text-sm"
+                >
+                  Enter your password
+                </label>
+              </div>
+            </div>
+
+            <div className="mt-1 p-2">
+              <select className="w-46 h-8 rounded-lg border border-[#d9dedd] bg-white px-4 py-1 text-[#333333] outline-none focus:shadow-[0_0_0_3px_rgba(15,129,122,0.15)] ">
+                <option value="">Select your Role</option>
+                <option value="customer">Customer</option>
+                <option value="seller">Seller</option>
+              </select>
+            </div>
+
+            <div className="mt-1 p-2">
+              <button
+                type="button"
+                className="w-80! cursor-pointer rounded-3xl! border border-[#0f817a] bg-[#0f817a] px-6 py-3 font-medium text-white hover:bg-[#0c6f69]"
+              >
+                Submit
+              </button>
+            </div>
+
           </div>
-        </div>
 
-        <div className="mt-1 p-2">
-          <div className="relative w-80">
-            <input
-              type="email"
-              id="email"
-              placeholder=" "
-              className="peer w-80 h-13 rounded-lg border border-[#d9dedd] bg-white px-4 py-3 text-[#333333] outline-none transition-all focus:border-[#0f817a] focus:shadow-[0px_0px_4px_rgba(15,129,122,0.25)]"
+          <div className="relative w-1/2 h-full overflow-hidden">
+            <img
+              src={image}
+              alt="Create account"
+              className="w-full max-h-150 object-cover"
             />
             
-            <label
-              htmlFor="email"
-              className="absolute left-4 top-1/2 -translate-y-1/2 bg-white px-1 text-[#777777] transition-all
-              peer-focus:top-0
-              peer-focus:text-sm
-              peer-focus:text-[#0f817a]
-              peer-not-placeholder-shown:top-0`
-              peer-not-placeholder-shown:text-sm"
-            >
-              Enter your email
-            </label>
           </div>
+
         </div>
-
-        <div className="mt-1 p-2">
-          <div className="relative w-80">
-            <input
-              type="password"
-              id="password"
-              placeholder=" "
-              className="peer w-80 h-13 rounded-lg border border-[#d9dedd] bg-white px-4 py-3 text-[#333333] outline-none transition-all focus:border-[#0f817a] focus:shadow-[0px_0px_4px_rgba(15,129,122,0.25)]"
-            />
-
-            <label
-              htmlFor="password"
-              className="absolute left-4 top-1/2 -translate-y-1/2 bg-white px-1 text-[#777777] transition-all
-              peer-focus:top-0
-              peer-focus:text-sm
-              peer-focus:text-[#0f817a]
-              peer-not-placeholder-shown:top-0`
-              peer-not-placeholder-shown:text-sm"
-            >
-              Enter your password
-            </label>
-          </div>
-        </div>
-
-        <div className="mt-1 p-2">
-          <div className="relative w-80">
-
-            <select className="w-40 h-8 rounded-lg border border-[#d9dedd] bg-white px-4 py-1 text-[#333333] outline-none appearance-none focus:border-[#0f817a] focus:shadow-[0px_0px_4px_rgba(15,129,122,0.25)]">
-              <option value="">Select you Role</option>
-              <option value="customer">customer</option>
-              <option value="Seller">Seller</option>
-            </select>
-
-          </div>
-        </div>
-
-        <div className="mt-1 p-2">
-          <button
-            type="button"
-            className="w-80! cursor-pointer rounded-3xl! border border-[#0f817a] bg-[#0f817a] px-6 py-3 font-medium text-white outline-none transition-all hover:bg-[#0c6f69] focus:shadow-[0px_0px_6px_rgba(15,129,122,0.35)] active:scale-[0.98]"
-          >
-            Submit
-          </button>
-        </div>
-
       </div>
-    </div>
-    
     </>
   );
 }
